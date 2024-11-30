ecd7b3cb7a54b37ddc66ae7352c79725b2c63238
bunyamin60 <126345264+bunyamin60@users.noreply.github.com>
2024-11-30 22:15:46 +0300

    deneme1

---
+++ Controllers/CalisanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebÖdev.Controllers
+{
+    public class CalisanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
